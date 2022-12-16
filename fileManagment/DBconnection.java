4a1b9bdfbfa45fdfc933f20ada8a0ddfefc812ff
Farha Dahman <farhajehad@gmail>
2022-12-16 21:28:51 +0200

    Edit the magic numbers

--- fileManagment/DBconnection.java
+++ fileManagment/DBconnection.java
@@ -1,14 +1,18 @@
 package fileManagment;
 import java.sql.*;
+
+
 public class DBconnection {
-    private String dbURL ="jdbc:mysql://localhost:3306/fileManagement";
-    private String username ="root";
-    private String password ="";
+
+    private static final String LINK_OF_DATABASE = "jdbc:mysql://localhost:3306/fileManagement";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "";
+
     private Connection connection;
     public void DBConnection(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(dbURL,username,password);
+            connection = DriverManager.getConnection(LINK_OF_DATABASE,USER_NAME,PASSWORD);
             if(connection!=null){
                 System.out.println("Success");}
             }catch (ClassNotFoundException e){
