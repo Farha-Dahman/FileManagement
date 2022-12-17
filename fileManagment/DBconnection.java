bf9c306c6effd18498f2508ca02f8f7e9e2eff5a
Raghad Khatatba <RaghadZKhatatba@gmail.com>
2022-12-17 11:20:21 +0200

    singleton pattern

--- fileManagment/DBconnection.java
+++ fileManagment/DBconnection.java
@@ -7,17 +7,32 @@ public class DBconnection {
     private static final String LINK_OF_DATABASE = "jdbc:mysql://localhost:3306/fileManagement";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "";
-
-    private Connection connection;
-    public void DBConnection(){
+    private static Connection connection= null;
+    public static Connection getConnection(){
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(LINK_OF_DATABASE,USER_NAME,PASSWORD);
-            if(connection!=null){
-                System.out.println("Success");}
-            }catch (ClassNotFoundException e){
-            e.printStackTrace();
-        } catch (SQLException e) {
+            if (connection == null) {
+                System.out.println("Wait in synchronized block");
+                synchronized (DBconnection.class) {//stop here ...
+                    if (connection == null) {// not null
+                        Class.forName("com.mysql.jdbc.Driver");
+                        connection = DriverManager.getConnection(LINK_OF_DATABASE, USER_NAME, PASSWORD);
+                        System.out.println("Success");
+                    }
+                }
+            }
+            return connection;
+        }catch (Exception e) {
+            System.out.println(e);
+            return connection;
+        }
+    }
+    private DBconnection() {
+    }
+    public static void Close(){
+        try {
+            DBconnection.getConnection().close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
     }
 }
--- fileManagment/main.java
+++ fileManagment/main.java
@@ -2,7 +2,7 @@ package fileManagment;
 
 public class main {
     public static void main(String[] args) {
-        DBconnection dBconnection = new DBconnection();
-        dBconnection.DBConnection();
+       DBconnection.getConnection();
+       DBconnection.Close();
     }
 }
