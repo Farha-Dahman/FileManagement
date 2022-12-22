d64f03740a3284c15c1620ae29de88792c072a5b
“Nama-Salamehgit config --global user.email “nama.salameh@gmail.com <92352860+Nama-Salameh@users.noreply.github.com>
2022-12-22 15:10:42 +0200

    Singleton principle

--- fileManagment/ImportingFiles/FilesImporter.java
+++ fileManagment/ImportingFiles/FilesImporter.java
@@ -18,70 +18,11 @@ public class FilesImporter {
             path = sc.next();
             System.out.println("please enter file type : ");
             fileType = sc.next();
-            File file = readingFiles(path);
+            File file = filesReader.readingFiles(path);
             System.out.println(" name : " + file.getName() + " size : " + file.length());
-            FilesImporter.importingInfoToDB(file, fileType, connection);
-            savingFiles(file, path);
+            importerToDB.importingInfoToDB(file, fileType, connection);
+            fileSaver.savingFiles(file, path);
             System.out.println("You need to exit program? (0,1) ");
             exit = sc.nextInt();}
     }
-
-    private static File readingFiles(String path) {
-        File file = null;
-        try {
-            file = new File(path);
-            Scanner myReader = new Scanner(file);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-        return file;
-    }
-
-    private static void savingFiles(File file, String path) {
-        FileOutputStream saveFile = null;
-        try {
-            saveFile = new FileOutputStream(file.getName());
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        ObjectOutputStream save = null;
-        try {
-            save = new ObjectOutputStream(saveFile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            save.writeObject(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            save.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void importingInfoToDB(File file, String type, Connection connection) {
-            try {
-                System.out.println("Inserting records into the table...");
-                String query = " insert into FILESINFO (name, type, size)" + " values (?, ?, ?)";
-                PreparedStatement preparedStmt = connection.prepareStatement(query);
-                preparedStmt.setString(1, file.getName());
-                preparedStmt.setString(2, type);
-                preparedStmt.setFloat(3, file.length());
-                //preparedStmt.setString(4, conent);
-                preparedStmt.execute();
-                System.out.println("success");
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
     }
