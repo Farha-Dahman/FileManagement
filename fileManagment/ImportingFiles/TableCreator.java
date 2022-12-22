cb331fae6b2e368946cd9f18c14d481a34c0d671
“Nama-Salamehgit config --global user.email “nama.salameh@gmail.com <92352860+Nama-Salameh@users.noreply.github.com>
2022-12-22 23:12:04 +0200

    adding content to DB

--- fileManagment/ImportingFiles/TableCreator.java
+++ fileManagment/ImportingFiles/TableCreator.java
@@ -6,9 +6,9 @@ import java.sql.Statement;
 
 public class TableCreator {
     public static void creatingTableForFilesInfo(Connection connection) {
-        try(Statement statement = connection.createStatement();){
+        try(Statement statement = connection.createStatement()){
 
-            String sql = "create table FILESINFO(id int(10) primary key not null AUTO_INCREMENT ,name varchar(15)  not null,type varchar(15),size float(4), version int(2),content varchar(100000))";
+            String sql = "create table FILESINFO(id int(10) primary key not null AUTO_INCREMENT ,name varchar(35)  not null,type varchar(15),size float(4), version int(2),content varchar(100000))";
 
             statement.executeUpdate(sql);
             System.out.println("Created table in given database...");
