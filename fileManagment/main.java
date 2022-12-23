71749b0530854d84abde953e7d20df183bda5187
Farha Dahman <farhajehad@gmail>
2022-12-23 12:28:32 +0200

    change on connection and main file

--- fileManagment/main.java
+++ fileManagment/main.java
@@ -6,14 +6,17 @@ import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Scanner;
+import fileManagment.Database.DBconnection;
+import fileManagment.FileRepository.ExportingFiles.ExportFile;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class main {
     public static void main(String[] args) throws IOException, SQLException {
-        Connection connection = fileManagment.DBconnection.getConnection();
+        Connection connection = DBconnection.getConnection();
         int version = 0;
         FilesImporter.importFiles(connection, version);
         FilesImporter.importFiles(connection, version);
-        fileManagment.DBconnection.Close();
 
         ExportFile exportfile = new ExportFile();
         exportfile.exportFile(connection);
