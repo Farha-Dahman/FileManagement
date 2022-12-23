e686c3013cd2e7a5684a461fabcc13fe38d55295
Farha Dahman <farhajehad@gmail>
2022-12-23 18:43:18 +0200

    adding the RollBack feature

---
+++ fileManagment/VersionControl/RollBack/LastVersion.java
@@ -0,0 +1,16 @@
+package fileManagment.VersionControl.RollBack;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LastVersion {
+    public int lastVersion(ResultSet resultSet) throws SQLException {
+        int max_version = 0;
+        while (resultSet.next()) {
+            if (resultSet.getInt("version") > max_version) {
+                max_version = resultSet.getInt("version");
+            }
+        }
+        return max_version;
+    }
+}
