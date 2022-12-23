38092cf66fc004c5e18007880109dc799255c7b0
Farha Dahman <farhajehad@gmail>
2022-12-23 15:38:15 +0200

    RollBack editing

--- fileManagment/VersionControl/RollBack/RollBack.java
+++ fileManagment/VersionControl/RollBack/RollBack.java
@@ -2,6 +2,9 @@ package fileManagment.VersionControl.RollBack;
 
 import fileManagment.FileRepository.ExportingFiles.Intf.InputInfo;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -9,15 +12,19 @@ import java.sql.SQLException;
 
 public class RollBack {
 
-    public void rollBack(Connection connection) throws SQLException {
+    public void rollBack(Connection connection) throws IOException {
         InputInfo inputInfo = new InsertFileInfo();
         ResultSet resultSet = inputInfo.insertInfo(connection);
 
 
-        Path pathOfFiles = Path.of("C:\\Users\\MASS\\FilesFromDB");
 
-        replaceVersion.replaceContent(pathOfFiles,resultSet);
+
+//        replaceVersion.replaceContent(pathOfFiles,resultSet);
 
     }
 
+
+
 }
+
+
--- fileManagment/main.java
+++ fileManagment/main.java
@@ -2,15 +2,20 @@ package fileManagment;
 
 import fileManagment.Database.DBconnection;
 import fileManagment.FileRepository.ExportingFiles.ExportFile;
+import fileManagment.VersionControl.RollBack.RollBack;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws IOException {
         Connection connection = DBconnection.getConnection();
 
-        ExportFile exportfile = new ExportFile();
-        exportfile.exportFile(connection);
+//        ExportFile exportfile = new ExportFile();
+//        exportfile.exportFile(connection);
+
+        RollBack rollBack=new RollBack();
+        rollBack.rollBack(connection);
     }
 }
