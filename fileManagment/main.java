f10b030e05af503e2180f013777e79f65efe0ca4
Farha Dahman <farhajehad@gmail>
2022-12-23 18:44:47 +0200

    adding the RollBack feature

--- fileManagment/main.java
+++ fileManagment/main.java
@@ -2,6 +2,7 @@ package fileManagment;
 
 import fileManagment.Database.DBconnection;
 import fileManagment.FileRepository.ExportingFiles.ExportFile;
+import fileManagment.VersionControl.RollBack.InsertFileInfo;
 import fileManagment.VersionControl.RollBack.RollBack;
 
 import java.io.IOException;
@@ -9,13 +10,13 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         Connection connection = DBconnection.getConnection();
 
 //        ExportFile exportfile = new ExportFile();
 //        exportfile.exportFile(connection);
 
-        RollBack rollBack=new RollBack();
+        RollBack rollBack = new RollBack();
         rollBack.rollBack(connection);
     }
 }
