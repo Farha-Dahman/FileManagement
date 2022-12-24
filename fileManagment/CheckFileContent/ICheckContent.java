fc48037ca0665f92ed9d4907f6b412a5d67e75d0
Farha Dahman <farhajehad@gmail>
2022-12-24 12:18:46 +0200

    delete the IsExist function

--- fileManagment/CheckFileContent/ICheckContent.java
+++
@@ -1,8 +0,0 @@
-package fileManagment.CheckFileContent;
-
-import java.util.ArrayList;
-
-public interface ICheckContent {
-
-    boolean fileIsExist(ArrayList<String> listOfFilesName, String nameFile);
-}
