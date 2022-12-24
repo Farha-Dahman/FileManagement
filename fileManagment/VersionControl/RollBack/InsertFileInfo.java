91db22e4c4ac2720ce08a9b2012151b4bb9cf6f8
Farha Dahman <farhajehad@gmail>
2022-12-24 09:26:56 +0200

    adding Insert File Info

---
+++ fileManagment/VersionControl/RollBack/InsertFileInfo.java
@@ -0,0 +1,29 @@
+package fileManagment.VersionControl.RollBack;
+
+import fileManagment.FileRepository.ExportingFiles.Intf.InputInfo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class InsertFileInfo implements InputInfo {
+    @Override
+    public ResultSet insertInfo(Connection connection) throws SQLException {
+        System.out.print("Input File Name: ");
+        Scanner fileName = new Scanner(System.in);
+        String nameFile = fileName.nextLine().trim();
+        System.out.print("Input File Type: ");
+        Scanner fileType = new Scanner(System.in);
+        String typeFile = fileType.nextLine().trim();
+
+        String query = "select * from files WHERE FileName = (?) and Type = (?)";
+        PreparedStatement prepareStatement = connection.prepareStatement(query);
+        prepareStatement.setString(1, nameFile);
+        prepareStatement.setString(2, typeFile);
+        ResultSet resultSet = prepareStatement.executeQuery();
+
+        return resultSet;
+    }
+}
