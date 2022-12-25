30c0bdd138a4108e4190a71ea3e372499decfe73
Raghad Khatatba <RaghadZKhatatba@gmail.com>
2022-12-25 17:07:47 +0200

    IdeleteFile

--- fileManagment/DeleteFile/IdeleteFile.java
+++ fileManagment/DeleteFile/IdeleteFile.java
@@ -4,8 +4,9 @@ import Exceptions.NullObjectException;
 import Exceptions.SQLQueryException;
 
 public interface IdeleteFile {
-        public void deleteByClassefication(String sql,String classification)throws SQLQueryException;
-        public void deleteFileByName(String FileName) throws NullObjectException, SQLQueryException;
-        public void deleteBycustomCategory(String nameClassification)throws SQLQueryException;
+    public void deleteFileByName(String FileName) throws NullObjectException, SQLQueryException;
+    public void deleteFileByType(String FileType) throws NullObjectException, SQLQueryException;
+    public void deleteFileBySize(String FileSize) throws NullObjectException, SQLQueryException;
+    public void deleteBycustomCategory(String nameClassification)throws SQLQueryException;
     }
 
